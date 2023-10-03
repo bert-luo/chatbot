9b75b400c7d6ff9d0968a4d7783a5466076411e4
Albert <luoa@mit.edu>
2023-10-03 11:38:02 -0700

    settings

--- server/creds/settings.py
+++ server/creds/settings.py
@@ -1,6 +1,15 @@
+import git
+import os
 
-ASTRA_DB_SECURE_BUNDLE_PATH = "/Users/albert/projects-2023/chatbot/server/creds/secure-connect-web-vectors.zip" 
-ASTRA_DB_TOKEN_JSON_PATH = "/Users/albert/projects-2023/chatbot/server/creds/astra-token.json" 
+def get_git_root():
+    git_repo = git.Repo(os.getcwd(), search_parent_directories=True)
+    git_root = git_repo.git.rev_parse("--show-toplevel")
+    return git_root
+
+ROOT = get_git_root()
+
+ASTRA_DB_SECURE_BUNDLE_PATH = ROOT + "/server/creds/secure-connect-web-vectors.zip" 
+ASTRA_DB_TOKEN_JSON_PATH = ROOT + "chatbot/server/creds/astra-token.json" 
 ASTRA_DB_KEYSPACE = "search" 
 
 METAPHOR_API_KEY = "7d3b0f92-903b-412c-905d-30c347c9b03f"
